0dcc9d9ed48dcee3c0b0bc6b762b9e9f19819e37
MichaelPogorelov <pogorellov@gmail.com>
2023-11-08 11:09:13 +0200

    save progress

--- Assets/CodeBase/Infrastructure/States/BootstrapState.cs
+++ Assets/CodeBase/Infrastructure/States/BootstrapState.cs
@@ -43,7 +43,7 @@ namespace CodeBase.Infrastructure.States
 			_services.RegisterSingle<IInputService>(ChooseInputService());
 			_services.RegisterSingle<IAssetProvider>(new AssetProvider());
 			_services.RegisterSingle<IPersistentProgressService>(new PersistentProgressService());
-			_services.RegisterSingle<ISaveLoadService>(new SaveLoadService());
+			_services.RegisterSingle<ISaveLoadService>(new SaveLoadService(_services.Single<IPersistentProgressService>(), _services.Single<IGameFactory>()));
 			_services.RegisterSingle<IGameFactory>(new GameFactory(_services.Single<IAssetProvider>()));
 		}
 
